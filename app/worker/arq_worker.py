2bfa07805be58b1e4dfefed80659c0216357db65
Chai-B <bansalchaitanya1234@gmail.com>
2026-05-12 18:11:29 +0530

    Configure ARQ worker Redis settings from env

--- app/worker/arq_worker.py
+++ app/worker/arq_worker.py
@@ -1,5 +1,9 @@
+from arq.connections import RedisSettings
+
+from app.core.config import settings
 from app.worker.tasks import process_resume
 
 
 class WorkerSettings:
     functions = [process_resume]
+    redis_settings = RedisSettings.from_dsn(settings.REDIS_URL)
